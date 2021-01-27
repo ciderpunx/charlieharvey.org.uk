4d8e3abbaacb4bb6f8ebdd326efd143d80e85b07
Charlie Harvey <>
2021-01-27 10:36:16 +0000

    Remove home page and spam 404 from popular block

--- lib/frontend/popular.pm
+++ lib/frontend/popular.pm
@@ -90,10 +90,11 @@ sub _popular_for {
                                 'Charlie Harvey : ','' ) AS title
                   FROM `piwik_log_link_visit_action`
                  WHERE `server_time` >= '$back_time'
-                    AND `server_time` <= '$today'
+                   AND `server_time` <= '$today'
                    AND `idsite`=1
                    AND `idaction_name` IS NOT NULL
-                  AND `idaction_name` > 0
+                   AND `idaction_name` > 0
+                   AND `idaction_name` NOT IN (7755,10510)
               GROUP BY `idaction_name`
               ORDER BY `unique_visits` DESC
                  LIMIT 10;"
